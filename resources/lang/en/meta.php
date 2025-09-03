8ee8b0fd728923e11cf1da88e5806c8e55e18888
marco76tv <marco.sottana@gmail.com>
2025-09-03 21:28:06 +0200

    .

--- resources/lang/en/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'description' => 'Modern and intuitive content management platform',
-    'keywords' => 'cms, content management, platform, web',
-    'default_title' => 'Home',
-];
--- resources/lang/it/meta.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'description' => 'Piattaforma di gestione contenuti moderna e intuitiva',
-    'keywords' => 'cms, gestione contenuti, piattaforma, web',
-    'default_title' => 'Home',
-];
